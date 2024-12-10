6cf5058d16b5fb6261bafa50ee01b8fbe4e90ab4
Tasnádi Zoltán <tasnadi98@gmail.com>
2024-12-10 08:09:32 +0200

    day 9

---
+++ src/days/day9.rs
@@ -0,0 +1,216 @@
+use std::{
+    collections::VecDeque,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+pub fn get_num_char(reader: BufReader<File>) -> Vec<u8> {
+    reader
+        .lines()
+        .next()
+        .expect("No lines")
+        .expect("Line read error")
+        .chars()
+        .map(|x| (x as u8 - '0' as u8))
+        .collect()
+}
+
+pub fn get_largest_id(nums: &[u8]) -> u64 {
+    ((nums.len() - 1) / 2) as u64
+}
+
+pub fn naive_solution(nums: &[u8]) -> u64 {
+    let mut v = VecDeque::new();
+    let mut id = 0;
+    for (i, n) in nums.iter().enumerate() {
+        if i % 2 == 1 {
+            for _ in 0..*n {
+                v.push_back(None);
+            }
+        } else {
+            for _ in 0..*n {
+                v.push_back(Some(id));
+            }
+            id += 1;
+        }
+    }
+    let mut sum = 0;
+    let mut pos = 0;
+
+    println!("{:?}", v);
+    //println!("{}", v.len());
+    while v.len() > 0 {
+        //println!("{}", v.len());
+        if v[0].is_some() {
+            println!("{} * {pos}", v[0].unwrap());
+            sum += v.pop_front().unwrap().unwrap() * pos;
+        } else {
+            while v.len() > 0 && v[v.len() - 1].is_none() {
+                v.pop_back();
+            }
+            if v.len() == 0 {
+                break;
+            }
+            println!("{} * {pos}", v[v.len() - 1].unwrap());
+            sum += v.pop_back().unwrap().unwrap() * pos;
+            v.pop_front();
+        }
+
+        pos += 1;
+    }
+
+    sum
+}
+
+pub fn smart_solution(nums: &[u8]) -> u64 {
+    let mut nums = nums.iter().cloned().collect::<Vec<u8>>();
+    let mut begin = 0;
+    let mut pos = 0;
+    let mut end = nums.len() - 1;
+    if end % 2 == 1 {
+        end -= 1;
+    }
+    let mut start_id = 0;
+    let mut end_id = get_largest_id(&nums);
+    let mut sum: u64 = 0;
+
+    while begin <= end {
+        //println!("{begin} {end}");
+        if begin % 2 == 1 {
+            if begin != end {
+                for _ in 0..nums[begin].min(nums[end]) {
+                    print!("({end_id}) * {pos} + ");
+                    sum += pos * end_id;
+                    pos += 1;
+                    //println!("{k}: {} : {}", nums[end], nums[begin]);
+                    nums[end] -= 1;
+                    nums[begin] -= 1;
+                }
+                if nums[end] == 0 {
+                    print!("||");
+                    end_id -= 1;
+                    end -= 2;
+                }
+                if nums[begin] == 0 {
+                    begin += 1;
+                    println!();
+                }
+            } else {
+                println!("{begin} {end}EEEE");
+                begin += 1;
+            }
+        } else {
+            for _ in 0..nums[begin] {
+                print!("({start_id}) * {pos} + ");
+                sum += pos * start_id;
+                pos += 1;
+                nums[begin] -= 1;
+            }
+            println!();
+            start_id += 1;
+            begin += 1;
+        }
+    }
+
+    sum
+}
+
+pub fn solution(reader: BufReader<File>) -> Result<u64, std::io::Error> {
+    let nums = get_num_char(reader);
+
+    Ok(smart_solution(&nums))
+}
+
+/* SOLUTION 2 */
+
+fn find_space_at_least_size_lt_j(
+    v: &VecDeque<Option<u64>>,
+    size: usize,
+    j: usize,
+) -> Option<usize> {
+    let mut k = 0;
+    //println!("AAA");
+    loop {
+        //println!("{k}, {j}, {size}");
+        while k < j && v[k].is_some() {
+            k += 1;
+        }
+        if k >= j {
+            return None;
+        }
+        let mut i = 0;
+        while v[k + i].is_none() {
+            i += 1;
+        }
+        if i >= size {
+            return Some(k);
+        } else {
+            k += i;
+        }
+    }
+}
+
+pub fn naive_solution_2(nums: &[u8]) -> u64 {
+    let mut v = VecDeque::new();
+    let mut id = 0;
+    for (i, n) in nums.iter().enumerate() {
+        if i % 2 == 1 {
+            for _ in 0..*n {
+                v.push_back(None);
+            }
+        } else {
+            for _ in 0..*n {
+                v.push_back(Some(id));
+            }
+            id += 1;
+        }
+    }
+
+    let mut i = v.len() - 1;
+
+    while i > 0 {
+        // Move to next not free space
+        while i > 0 && v[i].is_none() {
+            i -= 1;
+        }
+        if i == 0 {
+            break;
+        }
+        // Calculate size
+        let mut j = i;
+        while j > 0 && v[j].is_some() && v[j].unwrap() == v[i].unwrap() {
+            j -= 1;
+        }
+        j += 1;
+
+        // Calculate leftmost free space
+        let place = find_space_at_least_size_lt_j(&v, i - j + 1, j);
+        // Move
+        if let Some(k) = place {
+            for ii in 0..(i - j + 1) {
+                v.swap(k + ii, j + ii);
+            }
+        }
+        i = j - 1;
+    }
+    let mut sum = 0;
+    let mut pos = 0;
+
+    //println!("{:?}", v);
+    //println!("{}", v.len());
+    for x in v.iter() {
+        if let Some(val) = x {
+            sum += pos * val;
+        }
+
+        pos += 1;
+    }
+
+    sum
+}
+
+pub fn solution2(reader: BufReader<File>) -> Result<u64, std::io::Error> {
+    let nums = get_num_char(reader);
+
+    Ok(naive_solution_2(&nums))
+}
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ use std::{
 };
 
 use crate::days::{
-    day1, day2, day3, day4, day5, day6, day7,
-    day8, /*day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21,
-         day22, day23, day24, day25,       day9,*/
+    day1, day2, day3, day4, day5, day6, day7, day8,
+    day9, /*day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day20, day21,
+         day22, day23, day24, day25,       */
 };
 extern crate helper_lib;
 mod days;
 
 fn main() -> Result<(), io::Error> {
-    let day = 8;
+    let day = 9;
     let part = 2;
 
-    let f = File::open(format!("inputs/input{}", day))?;
+    let f = File::open(format!("inputs/input{}.txt", day))?;
     let reader = BufReader::new(f);
 
     print!("Day {day} (Part: {part}): ");
@@ -30,8 +30,8 @@ fn main() -> Result<(), io::Error> {
                 6 => println!("{}", day6::solution(reader).unwrap()),
                 7 => println!("{}", day7::solution(reader).unwrap()),
                 8 => println!("{}", day8::solution(reader).unwrap()),
-                /*9 => println!("{}", day9::solution(reader).unwrap()),
-                10 => println!("{}", day10::solution(reader).unwrap()),
+                9 => println!("{}", day9::solution(reader).unwrap()),
+                /*10 => println!("{}", day10::solution(reader).unwrap()),
                 11 => println!("{}", day11::solution(reader).unwrap()),
                 12 => println!("{}", day12::solution(reader).unwrap()),
                 13 => println!("{}", day13::solution(reader).unwrap()),
@@ -60,8 +60,8 @@ fn main() -> Result<(), io::Error> {
                 6 => println!("{}", day6::solution2(reader).unwrap()),
                 7 => println!("{}", day7::solution2(reader).unwrap()),
                 8 => println!("{}", day8::solution2(reader).unwrap()),
-                /*9 => println!("{}", day9::solution_2(reader).unwrap()),
-                10 => println!("{}", day10::solution_2(reader).unwrap()),
+                9 => println!("{}", day9::solution2(reader).unwrap()),
+                /*10 => println!("{}", day10::solution_2(reader).unwrap()),
                 11 => println!("{}", day11::solution_2(reader).unwrap()),
                 12 => println!("{}", day12::solution_2(reader).unwrap()),
                 13 => println!("{}", day13::solution_2(reader).unwrap()),
