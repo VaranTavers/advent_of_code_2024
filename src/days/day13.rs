efffd5b6791446142e41fbea9e8828ea53d25a8f
Tasnádi Zoltán <tasnadi98@gmail.com>
2024-12-14 12:37:54 +0200

    day13 and day14

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -3,21 +3,17 @@ use std::{
     io::{BufRead, BufReader, Lines},
 };
 
-pub fn gen_nums_from_line(line: &str, c: char) -> (usize, usize) {
+pub fn gen_nums_from_line(line: &str, c: char) -> (f64, f64) {
     let p_parts = line.split(c).collect::<Vec<&str>>();
     let x_parts = p_parts[1].split(',').collect::<Vec<&str>>();
 
     (
-        x_parts[0].parse::<usize>().unwrap(),
-        p_parts[2].parse::<usize>().unwrap(),
+        x_parts[0].parse::<f64>().unwrap(),
+        p_parts[2].parse::<f64>().unwrap(),
     )
 }
 
-pub fn get_line(
-    lines: &mut Lines<BufReader<File>>,
-    needle: &str,
-    op: char,
-) -> Option<(usize, usize)> {
+pub fn get_line(lines: &mut Lines<BufReader<File>>, needle: &str, op: char) -> Option<(f64, f64)> {
     let row = lines.next();
     if row.is_none() {
         return None;
@@ -33,15 +29,15 @@ pub fn get_line(
     Some(gen_nums_from_line(&row, op))
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct ClawMachine {
-    pub a: (usize, usize),
-    pub b: (usize, usize),
-    pub prize: (usize, usize),
+    pub a: (f64, f64),
+    pub b: (f64, f64),
+    pub prize: (f64, f64),
 }
 
 impl ClawMachine {
-    pub fn new(a: (usize, usize), b: (usize, usize), prize: (usize, usize)) -> Self {
+    pub fn new(a: (f64, f64), b: (f64, f64), prize: (f64, f64)) -> Self {
         Self { a, b, prize }
     }
 
@@ -58,7 +54,7 @@ impl ClawMachine {
     }
 }
 
-pub fn solution(reader: BufReader<File>) -> Result<usize, std::io::Error> {
+pub fn solution(reader: BufReader<File>) -> Result<f64, std::io::Error> {
     let mut lines = reader.lines();
     let mut machines = Vec::new();
 
@@ -69,15 +65,44 @@ pub fn solution(reader: BufReader<File>) -> Result<usize, std::io::Error> {
     }
 
     println!("{machines:?}");
-    let mut sum = 0;
+    let mut sum = 0.0;
+    for m in machines.iter().flatten() {
+        let b_val = (m.a.1 * m.prize.0 - m.a.0 * m.prize.1) / (m.a.1 * m.b.0 - m.b.1 * m.a.0);
+        let a_val = (m.prize.0 - b_val * m.b.0) / m.a.0;
+        println!("{a_val} {b_val}");
+        if a_val.fract() == 0.0 && b_val.fract() == 0.0 {
+            sum += a_val * 3.0 + b_val;
+        }
+    }
 
     Ok(sum)
 }
 
 /* SOLUTION 2 */
 
-pub fn solution2(reader: BufReader<File>) -> Result<usize, std::io::Error> {
-    let mut sum = 0;
+const VAL: f64 = 10000000000000.0;
+
+pub fn solution2(reader: BufReader<File>) -> Result<f64, std::io::Error> {
+    let mut lines = reader.lines();
+    let mut machines = Vec::new();
+
+    let mut m = ClawMachine::from_lines(&mut lines);
+    while m.is_some() {
+        machines.push(m.take());
+        m = ClawMachine::from_lines(&mut lines);
+    }
+
+    println!("{machines:?}");
+    let mut sum = 0.0;
+    for m in machines.iter().flatten() {
+        let b_val = (m.a.1 * (m.prize.0 + VAL) - m.a.0 * (m.prize.1 + VAL))
+            / (m.a.1 * m.b.0 - m.b.1 * m.a.0);
+        let a_val = (m.prize.0 + VAL - b_val * m.b.0) / m.a.0;
+        println!("{a_val} {b_val}");
+        if a_val.fract() == 0.0 && b_val.fract() == 0.0 {
+            sum += a_val * 3.0 + b_val;
+        }
+    }
 
     Ok(sum)
 }
---
+++ src/days/day14.rs
@@ -0,0 +1,173 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    str::FromStr,
+};
+
+#[derive(Debug, PartialEq)]
+pub struct Robot {
+    pub p: (i64, i64),
+    pub v: (i64, i64),
+}
+
+impl Robot {
+    pub fn new(p: (i64, i64), v: (i64, i64)) -> Self {
+        Self { p, v }
+    }
+
+    pub fn move_n(&mut self, n: i64, bounds: &(i64, i64)) {
+        let new_x = (self.p.0 + n * self.v.0 + n * bounds.0) % bounds.0;
+        let new_y = (self.p.1 + n * self.v.1 + n * bounds.1) % bounds.1;
+        self.p = (new_x, new_y);
+    }
+}
+
+impl FromStr for Robot {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let p_v = s.split_once(' ').unwrap();
+        let pside = p_v.0.split_once('=').unwrap();
+        let p_parts = pside.1.split_once(',').unwrap();
+        let vside = p_v.1.split_once('=').unwrap();
+        let v_parts = vside.1.split_once(',').unwrap();
+
+        Ok(Robot::new(
+            (p_parts.0.parse().unwrap(), p_parts.1.parse().unwrap()),
+            (v_parts.0.parse().unwrap(), v_parts.1.parse().unwrap()),
+        ))
+    }
+}
+
+pub fn print_debug_info(robots: &[Robot], bounds: &(i64, i64), blank_mid: bool) {
+    for j in 0..bounds.1 {
+        for i in 0..bounds.0 {
+            if (i == bounds.0 / 2 || j == bounds.1 / 2) && blank_mid {
+                print!(" ");
+            } else {
+                let s = robots.iter().filter(|x| x.p == (i, j)).count();
+                if s == 0 {
+                    print!(".");
+                } else {
+                    print!("{s}");
+                }
+            }
+        }
+        println!();
+    }
+}
+
+pub fn solution(reader: BufReader<File>) -> Result<i64, std::io::Error> {
+    let mut robots = reader
+        .lines()
+        .flatten()
+        .map(|line| Robot::from_str(&line))
+        .flatten()
+        .collect::<Vec<Robot>>();
+    let bounds = (101, 103);
+
+    let mut quadrants = [0; 4];
+    for robot in robots.iter_mut() {
+        robot.move_n(100, &bounds);
+        //println!("{robot:?}");
+        if robot.p.1 < bounds.1 / 2 {
+            if robot.p.0 < bounds.0 / 2 {
+                quadrants[0] += 1;
+            } else if robot.p.0 > bounds.0 / 2 {
+                quadrants[1] += 1;
+            }
+        } else if robot.p.1 > bounds.1 / 2 {
+            if robot.p.0 < bounds.0 / 2 {
+                quadrants[2] += 1;
+            } else if robot.p.0 > bounds.0 / 2 {
+                quadrants[3] += 1;
+            }
+        }
+    }
+
+    Ok(quadrants.iter().fold(1, |acc, val| acc * val))
+}
+
+/* SOLUTION 2
+
+
+pub fn is_symmetric(robots: &[Robot], bounds: &(i64, i64)) -> bool {
+    let mut a = vec![vec![false; bounds.0 as usize]; bounds.1 as usize];
+    for robot in robots.iter() {
+        a[robot.p.1 as usize][robot.p.0 as usize] = true;
+    }
+
+    for row in 0..(bounds.1 as usize) {
+        for col in 0..(bounds.0 / 2) as usize {
+            if a[row][col] != a[row][bounds.0 as usize - 1 - col] {
+                return false;
+            }
+        }
+    }
+    true
+}
+    */
+
+pub fn largest_continous_vertical_line(robots: &[Robot], bounds: &(i64, i64)) -> i64 {
+    let mut a = vec![vec![false; bounds.0 as usize]; bounds.1 as usize];
+    for robot in robots.iter() {
+        a[robot.p.1 as usize][robot.p.0 as usize] = true;
+    }
+
+    let mut max = 0;
+    for col in 0..(bounds.0 as usize) {
+        let mut val = 0;
+        for row in 0..(bounds.1) as usize {
+            if a[row][col] {
+                val += 1;
+            } else {
+                if val > max {
+                    max = val;
+                }
+                val = 0;
+            }
+        }
+    }
+    max
+}
+
+// Tried filled line through middle ran for:  202963000
+// Tried symmetric around center ran for: 32185000
+
+pub fn solution2(reader: BufReader<File>) -> Result<i64, std::io::Error> {
+    let mut robots = reader
+        .lines()
+        .flatten()
+        .map(|line| Robot::from_str(&line))
+        .flatten()
+        .collect::<Vec<Robot>>();
+    let bounds = (101, 103);
+
+    let mut i = 0;
+    let mut max = 0;
+    loop {
+        for robot in robots.iter_mut() {
+            robot.move_n(1, &bounds);
+        }
+        let val = largest_continous_vertical_line(&robots, &bounds);
+        if val > max {
+            max = val;
+            //break;
+            println!("{i}:");
+            print_debug_info(&robots, &bounds, false);
+        }
+        i += 1;
+        if i % 1000 == 0 {
+            println!("{i}");
+        }
+        if i == 20000 {
+            break;
+        }
+    }
+
+    println!("{i}:");
+    print_debug_info(&robots, &bounds, false);
+    println!();
+
+    Ok(6445)
+}
